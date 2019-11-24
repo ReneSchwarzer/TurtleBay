4a0cf120b81f56588b6e192a70558d15006ae283
Rene Schwarzer <rene_schwarzer@hotmail.de>
2019-11-24 09:56:22 +0100

    Einrichtung als Systemd-Service

--- src/core/deploy.sh
+++ src/core/deploy.sh
@@ -6,14 +6,19 @@ export DOTNET_ROOT=/usr/share/dotnet-sdk/
 dotnet build
 dotnet publish
 
+if [ sudo systemctl -q is-enabled turtlebay.service ] 
+then
+	sudo systemctl disable turtlebay.service 
+fi
+
 sudo mkdir -p /opt/turtlebay
 sudo chmod +x /opt/turtlebay
 #sudo rm -Rf /opt/turtlebay/*
+
+
 cp -Rf TurtleBay/bin/Debug/netcoreapp3.0/publish/* /opt/turtlebay
-cp run.sh /opt/turtlebay
-sudo chmod +x /opt/turtlebay/run.sh
+cp turtlebay.sh /opt/turtlebay
+sudo chmod +x /opt/turtlebay/turtlebay.sh
 
-sudo cp service.sh /etc/init.d/turtlebay
-sudo chmod +x /etc/init.d/turtlebay
-
-update-rc.d turtlebay defaults
+sudo cp turtlebay.service /etc/systemd/system
+sudo systemctl enable turtlebay.service
--- src/core/turtlebay.sh
+++ src/core/turtlebay.sh
@@ -4,7 +4,9 @@ export PATH=$PATH:/usr/share/dotnet-sdk/
 export DOTNET_ROOT=/usr/share/dotnet-sdk/ 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
+dir=´pwd´
+
 cd /opt/turtlebay
 ./TurtleBay
 
-cd ~/turtlebay
+cd $dir
